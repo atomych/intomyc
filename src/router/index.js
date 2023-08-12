ffb9551bd3bfbe517427cc6bbc5d171e6fe31caf
atomych <atomychtrue@gmail.com>
2023-08-12 15:45:35 +0300

    created first version of root system

--- src/router/index.js
+++ src/router/index.js
@@ -1,11 +1,57 @@
 import { createRouter, createWebHistory } from "vue-router";
-import HomeView from "../views/HomeView.vue";
+import StartView from "../views/StartView.vue";
+import UserView from "../views/UserView.vue";
+import EntryPage from "../pages/EntryPage.vue";
+import RegPage from "../pages/RegPage.vue";
+import MessPage from "../pages/MessPage.vue";
+import SearchPage from "../pages/SearchPage.vue";
+import SettPage from "../pages/SettPage.vue";
+import DialogPage from "../pages/DialogPage.vue";
 
 const routes = [
   {
     path: "/",
-    name: "home",
-    component: HomeView,
+    name: "view",
+    component: StartView,
+    children: [
+      {
+        path: "entry",
+        name: "entry",
+        component: EntryPage,
+      },
+      {
+        path: "registration",
+        name: "reg",
+        component: RegPage,
+      },
+      {
+        path: "user",
+        name: "user",
+        component: UserView,
+        children: [
+          {
+            path: "messages",
+            name: "mess",
+            component: MessPage,
+          },
+          {
+            path: "settings",
+            name: "sett",
+            component: SettPage,
+          },
+          {
+            path: "search",
+            name: "search",
+            component: SearchPage,
+          },
+          {
+            path: "dialog&id=:id",
+            name: "dialog",
+            component: DialogPage,
+          },
+        ],
+      },
+    ],
   },
 ];
 
