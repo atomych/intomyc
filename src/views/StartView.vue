6cedc06662119fbe8ff31b37218bff75eb8b3986
atomych <atomychtrue@gmail.com>
2023-08-13 15:11:07 +0300

    routing system

--- src/views/StartView.vue
+++ src/views/StartView.vue
@@ -1,9 +1,26 @@
 <template>
-  <router-view />
+  <div class="container">
+    <router-view />
+  </div>
 </template>
+
+<style lang="scss" scoped>
+.container {
+  display: flex;
+  justify-content: center;
+  align-items: center;
+  background-color: #ad6cff;
+
+  height: 100%;
+}
+</style>
 
 <script>
 export default {
   name: "StartView",
+
+  created() {
+    this.$router.push({ name: "entry" });
+  },
 };
 </script>
