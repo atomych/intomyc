a0d9d344adecb9359025d6b2e9ccff9b67ef0d6e
atomych <atomychtrue@gmail.com>
2023-08-13 23:55:42 +0300

    first disign for dialog list

--- src/pages/MessPage.vue
+++ src/pages/MessPage.vue
@@ -1,5 +1,45 @@
+<template>
+  <div class="container">
+    <dialog-item
+      :name="'user'"
+      :whoLast="'user'"
+      :unWatched="1"
+      :lastMess="'Hello'"
+    />
+    <dialog-item
+      :name="'user'"
+      :whoLast="'atomych'"
+      :unWatched="0"
+      :lastMess="'Hello2'"
+    />
+    <dialog-item
+      :name="'user'"
+      :whoLast="'user'"
+      :unWatched="0"
+      :lastMess="'Hello3'"
+    />
+  </div>
+</template>
+
+<style lang="scss" scoped>
+.container {
+  display: flex;
+  flex-direction: column;
+  width: 100%;
+  max-width: 800px;
+  margin: 0 auto;
+  padding: 20px;
+}
+</style>
+
 <script>
+import DialogItem from "../components/DialogItem.vue";
+
 export default {
   name: "MessPage",
+
+  components: {
+    DialogItem,
+  },
 };
 </script>
