5d4a9544c93fb7df6bacdaf99c06eb86df62e709
atomych <atomychtrue@gmail.com>
2023-08-16 05:53:35 +0300

    auto entry

--- src/pages/EntryPage.vue
+++ src/pages/EntryPage.vue
@@ -13,6 +13,10 @@
       v-model:fieldValue="password"
       class="field-out"
     />
+    <div class="save">
+      <input type="checkbox" ref="check" />
+      <div class="text">Remember me</div>
+    </div>
     <default-button :text="'Sign in'" @clickIn="login()" class="btn-out" />
     <default-link
       :text="'Create account'"
@@ -36,6 +40,20 @@
   margin-bottom: 35px;
 
   text-align: center;
+}
+
+.save {
+  display: flex;
+  justify-content: center;
+  align-items: center;
+  margin-bottom: 20px;
+  font-weight: 700;
+  font-size: 16px;
+  color: #000;
+
+  input {
+    margin-right: 10px;
+  }
 }
 
 .field-out {
@@ -78,13 +96,28 @@ export default {
     };
   },
 
+  created() {
+    const key = localStorage.getItem("AUTOENTRYKEY");
+    const auto = localStorage.getItem("checkbox");
+
+    if (key && auto) {
+      this.autoEntry({
+        key: key,
+        cb: () => {
+          this.$router.push({ name: "user" });
+        },
+      });
+    }
+  },
+
   methods: {
-    ...mapActions(["auth"]),
+    ...mapActions(["auth", "autoEntry"]),
 
     login() {
       if (this.email != "" && this.password.length >= 6) {
         this.auth({ email: this.email, password: this.password });
         this.$router.push({ name: "user" });
+        localStorage.setItem("checkbox", this.$refs.check.checked ? true : "");
       }
     },
 
